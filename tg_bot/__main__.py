9598271ef0d9f23d866d687f2ad8041d52cc5abf
Perfect_Maker <83399769+culprit-k@users.noreply.github.com>
2021-07-04 00:55:10 -0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -135,7 +135,15 @@ def start(bot: Bot, update: Update, args: List[str]):
 
             elif args[0][1:].isdigit() and "rules" in IMPORTED:
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
-               
+                
+    else:
+            first_name = update.effective_user.first_name
+            update.effective_message.reply_text(
+                PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID), 
+                
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
+                    [[InlineKeyboardButton(text="💢ᴊᴏɪɴ ᴏᴜʀ ɢʀᴏᴜᴘ💢", url="https://t.me/Movie_House_Group_2")]])
+                      
     else:
         update.effective_message.reply_text("ചത്തിട്ടില്ലാ...")
 
